fa7e472c57f2bb78be49f354f5f9e73706ba0a88
Giovani <giovanikx@gmail.com>
2018-09-24 19:52:36 -0300

    versao com multiplas conexoes

--- servidor.py
+++ servidor.py
@@ -55,12 +55,9 @@ def print_to_output_file(message):
 	f.write(message + '\n')
 
 # Representa o recebimento de uma mensagem e sua validação
-def receive_message(seqnum, message, md5, test_md5):
+def receive_message(client_key, seqnum, message, md5, test_md5):
 	global RWS
-	global NFE
-	global SW
-	global LFA
-	print("")
+	global sliding_windows
 	#print("Mensagem #{} recebida no servidor: {}".format(seqnum, message))
 	#print("RWS: {} NFE: {} LFA: {} SW: {}".format(RWS, NFE, LFA, SW))
 	# A mensagem só é relevante para nós se seu seqnum está dentro da janela e seu md5 é válido. Caso contrário, ela é descartada.
@@ -70,25 +67,26 @@ def receive_message(seqnum, message, md5, test_md5):
 	#checksum = create_md5(seqnum + sec + nsec + sz + message)
 	#pdb.set_trace()
 
-	if(valid_md5(md5, test_md5) and (seqnum <= LFA)):
+	if(valid_md5(md5, test_md5) and (seqnum <= sliding_windows[client_key]['LFA'])):
 		# Se a mensagem já foi ack, simplesmente retornamos True pra reenviar o ACK
-		if(seqnum < NFE):
+		if(seqnum < sliding_windows[client_key]['NFE']):
 			print("Mensagem {} já foi ACK".format(seqnum))
 			return True
+
 		# Adicionamos a mensagem na janela
-		SW[seqnum] = message
+		sliding_windows[client_key]['SW'][seqnum] = message
 		print("\nMensagem #{} adicionada à janela.".format(seqnum))
 
 		# Se o NFE agora tem uma mensagem, andamos com a janela.
-		while(SW[NFE] != ""):
-			print_to_output_file(SW[NFE])
-			NFE = NFE + 1
-			LFA = LFA + 1
-			SW.append("")
-			print("\nEscrevendo {} no arquivo -- Janela movida: {} – {} -> {} – {}".format(SW[NFE-1], NFE-1, LFA-1, NFE, LFA))
+		while(sliding_windows[client_key]['SW'][sliding_windows[client_key]['NFE']] != ""):
+			print_to_output_file(sliding_windows[client_key]['SW'][sliding_windows[client_key]['NFE']])
+			sliding_windows[client_key]['NFE'] = sliding_windows[client_key]['NFE'] + 1
+			sliding_windows[client_key]['LFA'] = sliding_windows[client_key]['LFA'] + 1
+			sliding_windows[client_key]['SW'].append("")
+			print("\nEscrevendo {} no arquivo -- Janela movida: {} – {} -> {} – {}".format(sliding_windows[client_key]['SW'][sliding_windows[client_key]['NFE']-1], sliding_windows[client_key]['NFE']-1, sliding_windows[client_key]['LFA']-1, sliding_windows[client_key]['NFE'], sliding_windows[client_key]['LFA']))
 
 		# Printa nova janela
-		print("\nRWS: {} NFE: {} LFA: {} SW: {}".format(RWS, NFE, LFA, SW))
+		print("\nNFE: {} LFA: {} SW: {}".format(sliding_windows[client_key]['NFE'], sliding_windows[client_key]['LFA'], sliding_windows[client_key]['SW']))
 
 		# Retornamos True para indicar que a mensagem foi válida (md5 correto e permitida na range da janela)
 		return True
@@ -96,13 +94,16 @@ def receive_message(seqnum, message, md5, test_md5):
 	# Retornamos False se a mensagem foi descartada
 	if(valid_md5(md5, test_md5) == False):
 		print("ERRO: MD5 inválido.")
-	if(seqnum > LFA):
-		print("ERRO: SeqNum ({}) > LFA({})".format(seqnum, LFA))
+	if(seqnum > sliding_windows[client_key]['LFA']):
+		print("ERRO: SeqNum ({}) > LFA({})".format(seqnum, sliding_windows[client_key]['LFA']))
 	return False
 
 
 def client_worker(client_socket, request):
 	global Perror
+
+	client_key = str(client_socket[0]) + ":" + str(client_socket[1])
+
 
 	#print("\n\nrequest CRUA recebida: {}".format(request))
 	seqnum = int.from_bytes(request[:8], byteorder='big')
@@ -122,7 +123,7 @@ def client_worker(client_socket, request):
 
 	#print("Request recebida @{}:\n seqnum: {} \n sec: {} \n nsec: {} \n sz: {} \n message: {} \n md5: {} ".format(client_socket, seqnum, sec, nsec, sz, message, md5))
 	# Se a mensagem foi recebida com sucesso, enviamos a confirmação
-	if(receive_message(seqnum, message, md5, test_md5) == True):
+	if(receive_message(client_key, seqnum, message, md5, test_md5) == True):
 		int_seqnum = seqnum
 		seqnum = (seqnum).to_bytes(8, byteorder = 'big')
 		sec = (sec).to_bytes(8, byteorder = 'big')
@@ -175,6 +176,7 @@ LFA = NFE + RWS - 1
 # Janela – Adicionamos +1 pois o indice começa em 1, e nao em 0
 SW = [""] * (RWS + 1)
 
+sliding_windows = dict()
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', port))
@@ -184,6 +186,17 @@ client_list = []
 # Aceita conexões c/ Threads
 while True:
 	request, client_socket = server_socket.recvfrom(16422)
+	
+	# Criamos o dicionario de dicionarios. A chave principal é a chave que identifica o cliente <host:port>
+	# e o dicionario interno contem a janela, o LFA e o NFE
+	client_key = str(client_socket[0]) + ":" + str(client_socket[1])
+	if client_key not in sliding_windows:
+		inner_dict = dict()
+		inner_dict['SW'] = SW
+		inner_dict['LFA'] = LFA
+		inner_dict['NFE'] = NFE
+		sliding_windows[client_key] = inner_dict
+
 	t = threading.Thread(target=client_worker, args=(client_socket, request, ))
 	client_list.append(t)
 	t.start()
